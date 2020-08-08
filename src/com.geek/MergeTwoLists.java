8a028158a59e9d496d06e51a89d084c2c9f55ce5
tr.wang <1362897840@qq.com.com>
2020-08-08 17:12:41 +0800

    链表反转,两数字和 ，删除链表的节点，删除链表倒数第n个节点，合并连个单链表

---
+++ src/com.geek/MergeTwoLists.java
@@ -0,0 +1,45 @@
+package com.geek;
+
+/**
+ * 合并两个链表，输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class MergeTwoLists {
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return null;
+        }
+        ListNode node = new ListNode(-1);
+        ListNode head = node;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                node.next = l1;
+                node = node.next;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                node = node.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) {
+            node.next = l2;
+        } else {
+            node.next = l1;
+        }
+        return head.next;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
---
+++ src/com.geek/ReversePrintList.java
@@ -0,0 +1,32 @@
+package com.geek;
+
+/**
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ */
+public class ReversePrintList {
+    public int[] reversePrint(ListNode head) {
+        ListNode node = head;
+        int count = 0;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        int[] array = new int[count];
+        node = head;
+        for (int i = count - 1; i >= 0; i--) {
+            array[i] = node.val;
+            node = node.next;
+            count--;
+        }
+        return array;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
